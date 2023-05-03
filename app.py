8dd704a6fb56eac00c0fa3b523de042c4cefca15
Lisa Kalfhues <l.kalfhues@gmail.com>
2023-05-03 10:36:14 +0200

    updates on movie_recommender

--- app.py
+++ app.py
@@ -9,16 +9,6 @@ app = Flask(__name__)
 def homepage():
     return render_template('home.html')
 
-#@app.route('/')
-# def recommender():
-    # top_two = random_recommender()
-    # return f'{top_two}'  
-    
-# @app.route('/results')
-# def recommender():
-#     print(request.args)
-#     return f'Here are some recommended movies: {random_recommender()}'
-
 @app.route('/results')
 def recommender():
     user_query = request.args.to_dict()
@@ -26,11 +16,7 @@ def recommender():
     top_two = random_recommender()
     return render_template('results.html', movies = top_two)
 
-
 if __name__ == '__main__':
     app.run(port=5002, debug=True)
 
-
-# expand brower by that:
-# ?t=ffab&q=in+the+mouth+of+madness&ia=web
 
